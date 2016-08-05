14ec5e0770018b32d74e027d1143c8eab4a26e46
Tally Barak <tally.barak@gmail.com>
2016-08-05 09:32:53 +0300

    Initial commit

---
+++ client/imports/components/socially/socially.js
@@ -0,0 +1,90 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import ngMaterial from 'angular-material';
+import ngSanitize from 'angular-sanitize';
+import uiRouter from 'angular-ui-router';
+import 'ionic-sdk/release/js/ionic';
+import 'ionic-sdk/release/js/ionic-angular';
+import 'ionic-sdk/release/css/ionic.css';
+
+//import * as vis from 'ui-router-visualizer';
+
+import { Meteor } from 'meteor/meteor';
+
+import webTemplate from './web.html';
+import mobileTemplate from './mobile.html';
+import { name as PartiesList } from '../partiesList/partiesList';
+import { name as PartyDetails } from '../partyDetails/partyDetails';
+import { name as Navigation } from '../navigation/navigation';
+import { name as Auth } from '../auth/auth';
+
+
+var vis = require('ui-router-visualizer');
+
+class Socially {}
+
+const name = 'socially';
+const template = Meteor.isCordova ? mobileTemplate : webTemplate;
+
+// create a module
+export default angular.module(name, [
+  angularMeteor,
+  ngMaterial,
+  ngSanitize,
+  uiRouter,
+  vis,
+  PartiesList,
+  PartyDetails,
+  Navigation,
+  Auth,
+  'accounts.ui',
+  'ionic'
+]).component(name, {
+  template,
+  controllerAs: name,
+  controller: Socially
+})
+  .config(config)
+  .run(run);
+
+function config($locationProvider, $urlRouterProvider, $mdIconProvider) {
+  'ngInject';
+
+  $locationProvider.html5Mode(true);
+
+  $urlRouterProvider.otherwise('/parties');
+
+  const iconPath =  '/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/';
+
+  $mdIconProvider
+    .iconSet('social',
+      iconPath + 'svg-sprite-social.svg')
+    .iconSet('action',
+      iconPath + 'svg-sprite-action.svg')
+    .iconSet('communication',
+      iconPath + 'svg-sprite-communication.svg')
+    .iconSet('content',
+      iconPath + 'svg-sprite-content.svg')
+    .iconSet('toggle',
+      iconPath + 'svg-sprite-toggle.svg')
+    .iconSet('navigation',
+      iconPath + 'svg-sprite-navigation.svg')
+    .iconSet('image',
+      iconPath + 'svg-sprite-image.svg');
+}
+
+function run($rootScope, $state, ng1UIRouter) {
+  'ngInject';
+
+  vis.visualizer(ng1UIRouter);
+
+  $rootScope.$on('$stateChangeError',
+    (event, toState, toParams, fromState, fromParams, error) => {
+      if (error === 'AUTH_REQUIRED') {
+        $state.go('parties');
+      }
+    }
+  );
+}
+
+
---
+++ client/main.js
@@ -0,0 +1,20 @@
+import angular from 'angular';
+
+import { Meteor } from 'meteor/meteor';
+
+import { name as Socially } from './imports/components/socially/socially';
+
+
+function onReady() {
+  angular.bootstrap(document, [
+    Socially
+  ], {
+    strictDi: true
+  });
+}
+
+if (Meteor.isCordova) {
+  angular.element(document).on('deviceready', onReady);
+} else {
+  angular.element(document).ready(onReady);
+}
